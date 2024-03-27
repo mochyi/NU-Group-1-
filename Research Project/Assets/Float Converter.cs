00dde2c3c43435507b27c9b71aa1709c79614ed0
Mocha <marcus.mamado@gmail.com>
2024-03-27 19:39:45 +0800

    button converting works

--- Research Project/Assets/Float Converter.cs
+++ Research Project/Assets/Float Converter.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,29 +15,21 @@ public class FloatConverter : MonoBehaviour
 
     public float myFloat;
 
-    public 
     
+
+
     
-    // Start is called before the first frame update
-    void Start()
+    public void whenButtonClicked()
     {
-        
+        float.TryParse(myString, out myFloat);
     }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-       if (float.TryParse(myString, out myFloat))
-        {
 
-            Debug.Log("Convert Success");
 
-        }
-       else
-        {
-            Debug.Log("Couldn't Be Converted");
-        }
 
-    }
+
+
+
+
 }
---
+++ Research Project/Assets/Original Flot COnverter.cs
@@ -0,0 +1,42 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OriginalFlotCOnverter : MonoBehaviour
+{
+
+    public string myString;
+
+    public float myFloat;
+
+    public 
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       if (float.TryParse(myString, out myFloat))
+        {
+
+            Debug.Log("Convert Success");
+
+        }
+       else
+        {
+            Debug.Log("Couldn't Be Converted");
+        }
+
+    }
+}
